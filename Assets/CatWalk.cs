37b1317d24ea42fe76e76409b5d45759e98608db
tbodzak <tomaszbodzak@gmail.com>
2018-09-08 20:11:37 +0200

    ver 0.7
    harts and time

--- Assets/CatWalk.cs
+++ Assets/CatWalk.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CatWalk : MonoBehaviour {
+public class CatWalk : MonoBehaviour
+{
 
     public Rigidbody2D rb;
 
@@ -11,62 +12,66 @@ public class CatWalk : MonoBehaviour {
     public float maxSpeed = 14f;
     public float MaxUpPos;
     public float MaxDownPos;
-	public float MaxUpPos2;
+    public float MaxUpPos2;
     public float MaxDownPos2;
     public int licz = 5;
+    public static int Przelicznikpredkosci = 1;
 
-    float speed = 1f;
+    public static float speed = 1f;
 
-     void Start()
+    void Start()
     {
-       //  speed = Random.Range(minSpeed, 14f);
-       // speed = 1f;
+
+        //  speed = Random.Range(minSpeed, 14f);
+        // speed = 1f;
     }
     Vector3 direction = Vector3.up;
 
     void FixedUpdate()
     {
         transform.position += direction * Time.deltaTime * speed;
-	if (CatSpa.randomIndex < 2){
-        if (transform.position.y >= MaxUpPos)
+
+        if (CatSpa.randomIndex < 3)
         {
-            direction = Vector3.down;
-            GameControlScript.health -= 1;
-             }
-      if ( Score.scoreValue > licz)
+            if (transform.position.y >= MaxUpPos)
+            {
+                direction = Vector3.down;
+                 GameControlScript.health -= 1;
+            }
+
+            if (transform.position.y <= MaxDownPos)
+                Destroy(gameObject);
+
+        }
+        else if (CatSpa.randomIndex >= 3)
         {
-            speed += 0.2f;
-            licz += 5;
+            if (transform.position.y >= MaxUpPos2)
+            {
+                direction = Vector3.down;
+                GameControlScript.health -= 1;
+            }
+
+            if (transform.position.y <= MaxDownPos2)
+                Destroy(gameObject);
         }
 
-        if (transform.position.y <= MaxDownPos)
-            Destroy(gameObject);
-
-}
-	else 
-		if (CatSpa.randomIndex >=2)
-		{ 
-	if (transform.position.y >= MaxUpPos2)
+        if (Score.scoreValue > licz)
         {
-            direction = Vector3.down;
-            GameControlScript.health -= 1;
-             }
-      if ( Score.scoreValue > licz)
-        {
-            speed += 0.2f;
-            licz += 5;
+            speed += 0.8f;
+            licz += 2;
+            Przelicznikpredkosci += 1;
         }
+        // Debug.Log(Score.scoreValue + "      " + licz);
+        Debug.Log(speed);
+        Debug.Log(Przelicznikpredkosci);
 
-        if (transform.position.y <= MaxDownPos2)
-            Destroy(gameObject);
-}
 
-      //  Debug.Log(transform.position.y);
+        //Debug.Log(transform.position.y);
     }
 
 
     void OnMouseDown()
-   {
+    {
 
         if (direction == Vector3.up)
         {
@@ -74,10 +79,10 @@ public class CatWalk : MonoBehaviour {
             var audioSource = GetComponent<AudioSource>();
             audioSource.Play();
         };
-                
+
         direction = Vector3.down;
 
-       
+
 
     }
 }
--- Assets/Scripts/PowerupsMenu.cs
+++ Assets/Scripts/PowerupsMenu.cs
@@ -3,10 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PowerupsMenu : MonoBehaviour {
+public class PowerupsMenu : MonoBehaviour
+{
 
     public CoinAmmount coin; //Stan konta/liczba kredytów
-    public int Hearts = 0; //Życie
     public int HeartsMax = 3; //Maksymalna liczba żyć
     public int GameSpeed = 0; //Prędkość gry
     public int GameSpeedMax = 9; //Maksymalna prędkość gry
@@ -14,38 +14,36 @@ public class PowerupsMenu : MonoBehaviour {
     public int cost2; //Koszt GameSpeed
     public Text HeartsText; //Wyświetla liczbę żyć
     public Text GameSpeedText; //Wyświetla aktualną prędkość gry
+    string licz = "0";
 
     // Use this for initialization
-    void Start ()
+    void Start()
     {
+        GameControlScript.health = 3;
         coin.coinNumber = PlayerPrefs.GetInt("Coins,0"); //Rekuperacja ilośći kredytów
-        Hearts = PlayerPrefs.GetInt("Hearts", 0); //Rekuperacja ilośći żyć
-        GameSpeed = PlayerPrefs.GetInt("GameSpeed", 0); //Rekuperacja prędkości gry
-        UpdateHeartsText(); //Funkcja do pokazania ilości żyć
-        UpdateGameSpeedText(); //Funckja do pokazania prędkości gry
+    }
+
+
+
+
+    public void Update()
+    {
+        HeartsText.text = GameControlScript.health.ToString();
+        GameSpeedText.text = CatWalk.Przelicznikpredkosci.ToString();
 
     }
-	
-	// Update is called once per frame
-	public void UpdateHeartsText ()
-    {
-        HeartsText.text = Hearts.ToString(); //String używany do update'u życia
-	}
 
-    public void UpdateGameSpeedText()
-    {
-        GameSpeedText.text = GameSpeed.ToString(); //String używany do update'u prędkości gry
-    }
+
 
     public void BuyHearts() //Funkcja kupowania życia
     {
-        if(coin.coinNumber >= cost1) //Sprawdzenie czy liczba kredytów za "Hearts" jest wystarczająca
+        if (coin.coinNumber >= cost1) //Sprawdzenie czy liczba kredytów za "Hearts" jest wystarczająca
         {
-            if(Hearts < HeartsMax)
+
+            if (GameControlScript.health < HeartsMax)
             {
                 coin.coinNumber -= cost1; //Odejmowanie kredytów za "Hearts" od całkowitej liczy kredytów
-                Hearts++;
-                UpdateHeartsText(); //Zwiększa się +1
+                GameControlScript.health++;
             }
             else
             {
@@ -56,18 +54,16 @@ public class PowerupsMenu : MonoBehaviour {
 
     public void BuyGameSpeed()
     {
-        if (GameSpeed < GameSpeedMax)
+        if (CatWalk.Przelicznikpredkosci < GameSpeedMax && CatWalk.Przelicznikpredkosci > 1 && coin.coinNumber >= 200)
         {
             coin.coinNumber -= cost2;
-            GameSpeed++;
-            UpdateGameSpeedText();
-        }  
+            CatWalk.speed -= 1.8f;
+            CatWalk.Przelicznikpredkosci -= 1;
+        }
     }
 
     void OnDestroy() //Zapisywanie z PlayerPrefs
     {
-        PlayerPrefs.SetInt("Hearts", Hearts);
-        PlayerPrefs.SetInt("GameSpeed", GameSpeed);
         PlayerPrefs.SetInt("Coins", coin.coinNumber);
         PlayerPrefs.Save();
     }
